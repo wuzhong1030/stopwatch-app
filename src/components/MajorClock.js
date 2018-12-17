6e3853a44ff3fbcdec614592aae6a81d944a21a2
wuzhong1598@qq.com <wuzhong1598@qq.com>
2018-12-17 13:47:33 +0800

    first commit

---
+++ src/components/MajorClock.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ms2Time } from "../utils";
+
+const MajorClock = ({ milliseconds = 0 }) => {
+  return <h1>{ms2Time(milliseconds)}</h1>;
+};
+
+export default MajorClock;
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+import padStart from 'lodash/padStart';
+
+export const ms2Time = (milliseconds) => {
+  let time = milliseconds;
+  const ms = milliseconds % 1000;
+  time = (milliseconds - ms) / 1000;
+  const seconds = time % 60;
+  time = (time - seconds) / 60;
+  const minutes = time % 60;
+  const hours = (time - minutes) / 60;
+
+  const result = padStart(hours, 2, '0') + ":" + padStart(minutes, 2, '0') + ":" + padStart(seconds, 2, '0') + "." + padStart(ms, 3, '0');
+  return result;
+}
